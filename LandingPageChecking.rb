a92de6b0a91b245dcfaa2c19940fa1c377db6627
sebink <sebink@gmail.com>
2014-02-17 18:54:48 -0500

    Update LandingPageChecking.rb

--- LandingPageChecking.rb
+++ LandingPageChecking.rb
@@ -37,7 +37,7 @@ describe "LandingPageChecking" do
 
     @driver.get(@base_url + "/b/site/index.html")
 
-    verify { (@driver.find_element(:xpath, "//*[@id='topBar']/div/div[1]/ul[2]/li[3]/a").text).should == "SIGN UP" }
+    verify { (@driver.find_element(:xpath, "//*[@id='topBar']/div/div[1]/ul[2]/li[3]/a").text).should == "SIGN U12P" }
     verify { (@driver.find_element(:xpath, "//*[@id='topBar']/div/div[1]/ul[2]/li[4]/a").text).should == "LOG IN" }
     end
   
