d300bb5c14eaed3f50a8d90cc3a10b0ea55b890a
sebink <sebink@gmail.com>
2014-03-29 16:27:49 -0400

    Update LandingPageChecking.rb

--- LandingPageChecking.rb
+++ LandingPageChecking.rb
@@ -21,7 +21,7 @@ describe "LandingPageChecking" do
   #}
 #}
 #Selenium::WebDriver::Safari.path = "/Users/Shared/Jenkins/Home/Safari.app/Contents/MacOS/safari"
-      @driver = Selenium::WebDriver.for :remote, :desired_capabilities => :chrome
+      @driver = Selenium::WebDriver.for :chrome#remote, :desired_capabilities => :chrome
 
         #@driver = Selenium::WebDriver.for :firefox
         @driver.manage().window().maximize()
