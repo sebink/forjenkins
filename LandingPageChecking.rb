5a3fd576df59ef1bf710146bd60f67ec91f0ba18
sebink <sebink@gmail.com>
2014-03-30 23:21:22 -0400

    Update LandingPageChecking.rb

--- LandingPageChecking.rb
+++ LandingPageChecking.rb
@@ -20,8 +20,8 @@ describe "LandingPageChecking" do
    # :default_directory => "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome2323"
   #}
 #}
-#Selenium::WebDriver::Safari.path = "/Users/Shared/Jenkins/Home/Safari.app/Contents/MacOS/safari"
-      @driver = Selenium::WebDriver.for(:remote, :url => "http://127.0.0.1:5555/wd/hub")
+Selenium::WebDriver::Safari.path = "/Users/Shared/Jenkins/Library/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+      @driver = Selenium::WebDriver.for :chrome
 
         #@driver = Selenium::WebDriver.for :firefox
         @driver.manage().window().maximize()
